c721a04e83264c9beaaae013c0007c0b93a5b9b2
ben <ben@gmail.com>
2017-06-13 17:58:53 +0200

    dernier commit de mardi

--- web/themes/custom/project/js/main.js
+++ web/themes/custom/project/js/main.js
@@ -1,17 +1,31 @@
+function log(...data) {
+    console.log(...data);
+}
 (function($) {
-    $(document).ready(function() {
-//        console.log("test");
-        $('.views-row').hide();
-    });
+//    $(document).ready(function() {
+////        console.log("test");
+//        $('.view-liste-des-avis .views-row a').hide();
+//    });
     $('h3').on('click', e => {
         console.log("click");
 //        let target = $(e.target);
 //        target.siblings().toggle(1000);
-        let view = $('.views-row');
+        let view = $('div.field-content a');
+        console.log(view);
         for (let i = 0; i < view.length; i++) {
-            console.log("test");
-//            view + i.toggle(1000);
+            console.log(view.length);
+//            let test = view[i];
+//            test.toggle(1000);
+//            console.log(target[i]);
+//            let count = view + i;
+//            console.log(count);
+//            count.toggle(1000);
+//            $(target[i]).on('click', e => {
+//                view[i].toggle(1000);
+//            });
+
         }
+
     });
 
 })(jQuery);
